9664d757b130c42dceb655ca0f86fdf6cede66ee
muhammed-alateeqi1 <youremail@example.com>
2025-05-17 03:57:08 +0300

    slider feature at product-details-component

--- src/app/layout/additions/product-details/product-details.component.ts
+++ src/app/layout/additions/product-details/product-details.component.ts
@@ -2,36 +2,56 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../../shared/services/product/product.service';
 import { product } from '../../../shared/interfaces/product';
 import { ActivatedRoute } from '@angular/router';
+import { CarouselModule , OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent implements OnInit {
-  product!: product;
-constructor(private _ProductService:ProductService , private _ActivatedRoute:ActivatedRoute){}
-  
-  ngOnInit(): void {
-      this.getProductById();
+    customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+
+    },
+    nav: true
   }
-  getProductById(){
-    let id!:string;
+  isLoading: boolean = false;
+  product!: product;
+  constructor(private _ProductService: ProductService, private _ActivatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getProductById();
+  }
+  getProductById() {
+    this.isLoading = true;
+    let id!: string;
     this._ActivatedRoute.params.subscribe({
-      next : product=>{
+      next: product => {
         id = product['productId'];
         // console.log(product['productId']);
       },
-      error: err =>{
+      error: err => {
         console.log(err.message);
       }
     })
     this._ProductService.getProductById(id).subscribe({
-      next : res =>{
+      next: res => {
         this.product = res.data;
         console.log(res.data);
+        this.isLoading = false;
       }
     })
   }
