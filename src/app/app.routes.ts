01e6a762340f46e80172a3e446599fc2ce3de0a3
muhammed-alateeqi1 <youremail@example.com>
2025-07-26 22:48:01 +0300

    feat:apply lazyloading concept on standalone components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { NotfoundComponent } from './layout/additions/notfound/notfound.component';
-import { BrandsComponent } from './layout/pages/brands/brands.component';
-import { CartComponent } from './layout/pages/cart/cart.component';
+// import { BrandsComponent } from './layout/pages/brands/brands.component';
+// import { CartComponent } from './layout/pages/cart/cart.component';
 import { HomeComponent } from './layout/pages/home/home.component';
 import { LoginComponent } from './layout/pages/login/login.component';
-import { ProductsComponent } from './layout/pages/products/products.component';
+// import { ProductsComponent } from './layout/pages/products/products.component';
 import { RegisterComponent } from './layout/pages/register/register.component';
 import { CategoriesComponent } from './layout/pages/categories/categories.component';
 import { authGuard } from './shared/guards/auth.guard';
@@ -20,10 +20,10 @@ import { CategoryProductsComponent } from './layout/pages/category-products/cate
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-    { path: 'cart', component: CartComponent, canActivate: [authGuard] },
-    { path: 'categories', component: CategoriesComponent, canActivate: [authGuard] },
-    { path: 'products', component: ProductsComponent, canActivate: [authGuard] },
-    { path: 'brands', component: BrandsComponent, canActivate: [authGuard] },
+    { path: 'cart', loadComponent : ()=> import('./layout/pages/cart/cart.component').then((c)=> c.CartComponent), canActivate: [authGuard] },
+    { path: 'categories', loadComponent : ()=>import('./layout/pages/categories/categories.component').then((c)=>c.CategoriesComponent), canActivate: [authGuard] },
+    { path: 'products', loadComponent : ()=> import('./layout/pages/products/products.component').then((c)=> c.ProductsComponent), canActivate: [authGuard] },
+    { path: 'brands', loadComponent : ()=> import('./layout/pages/brands/brands.component').then((c)=> c.BrandsComponent), canActivate: [authGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'forgetpassword', component: ForgetpasswordComponent },
--- src/app/layout/pages/brands/brands.component.ts
+++ src/app/layout/pages/brands/brands.component.ts
@@ -5,29 +5,30 @@ import { BrandService } from '../../../shared/services/brands.service';
 import { BrandsRes, Daum } from '../../../shared/interfaces/BrandsRes';
 
 @Component({
+  standalone: true,
   selector: 'app-brands',
   templateUrl: './brands.component.html',
   styleUrls: ['./brands.component.css']
 })
 export class BrandsComponent implements OnInit {
-  brands: Daum[] = []; 
+  brands: Daum[] = [];
   loading: boolean = true;
 
-  constructor(private _BrandService: BrandService) {}
+  constructor(private _BrandService: BrandService) { }
 
   ngOnInit(): void {
-    if(typeof localStorage != "undefined"){
-      localStorage.setItem('currentPage','/brands')
+    if (typeof localStorage != "undefined") {
+      localStorage.setItem('currentPage', '/brands')
+    }
+    this.getAllbrands()
   }
-   this.getAllbrands()
-  }
-  getAllbrands(){
-        this._BrandService.getAllBrands().subscribe({
+  getAllbrands() {
+    this._BrandService.getAllBrands().subscribe({
       next: (res: BrandsRes) => {
         this.brands = res.data;
         this.loading = false;
         console.log(res.data);
-        
+
       },
       error: (err) => {
         console.error(err);
